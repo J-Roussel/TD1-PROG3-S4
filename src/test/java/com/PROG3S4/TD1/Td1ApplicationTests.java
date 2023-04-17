1dac37d3c662904f939f66424963602a8057ebb3
Roussel Rakoto <hei.roussel11@gmail.com>
2023-04-17 16:01:17 +0300

    first commit

---
+++ src/test/java/com/PROG3S4/TD1/Td1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PROG3S4.TD1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
